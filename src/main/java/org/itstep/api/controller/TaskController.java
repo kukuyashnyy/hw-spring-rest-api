c63491651a30f61491b3525541a8012582cfab67
V. Kukuyashnyy <starsonic71@gmail.com>
2021-03-01 22:36:28 +0200

    update

--- src/main/java/org/itstep/api/controller/TaskController.java
+++ src/main/java/org/itstep/api/controller/TaskController.java
@@ -1,14 +1,21 @@
 package org.itstep.api.controller;
 
-import jdk.jfr.internal.RequestEngine;
 import org.itstep.entity.Task;
 import org.itstep.repository.TaskRepository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.HashMap;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/todolist")
@@ -19,8 +26,36 @@ public class TaskController {
         this.taskRepository = taskRepository;
     }
 
+    @GetMapping
+    public ResponseEntity<?> allTask(@RequestParam(required = false, defaultValue = "20") int size,
+                                     @RequestParam(required = false, defaultValue = "1") int page,
+                                     @RequestParam(required = false) String sort) {
+        System.out.println("size = " + size + ", page = " + page + ", sort = " + sort);
+
+        try {
+            Pageable pageable;
+            if (sort == null){
+                pageable = PageRequest.of(page, size);
+            } else {
+                pageable = PageRequest.of(page, size, Sort.by(sort));
+            }
+
+            Page<Task> pageTasks = taskRepository.findAll(pageable);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("tasks", pageTasks.getContent());
+            response.put("currentPage", pageTasks.getNumber());
+            response.put("totalItems", pageTasks.getTotalElements());
+            response.put("totalPages", pageTasks.getTotalPages());
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
     @GetMapping(path = "{id}")
-    public ResponseEntity<Task> carById(@PathVariable int id) {
+    public ResponseEntity<Task> taskById(@PathVariable int id) {
         Task task = taskRepository.findById(id).orElse(null);
         if (task != null) {
             return ResponseEntity.ok(task);
